82905fa469739a1104e4d61aeca435245f788976
Yara <cremaschi.yara@gmail.com>
2023-11-08 11:52:40 +0000

    added more logic

--- snake-game/index.js
+++ snake-game/index.js
@@ -58,3 +58,91 @@ Block.prototype.checkPosition = function (otherBlock) {
   return this.col === otherBlock.col && this.row === otherBlock.row;
 };
 
+const Snake = function () {
+  this.segments = [new Block(7, 5), new Block(6, 5), new Block(5, 5)];
+
+  this.direction = "right";
+  this.nextDirection = "right";
+};
+
+Snake.prototype.draw = () => {
+  for (let i = 0; i < this.segments.length; i++) {
+    this.segments[i].drawSquare("Blue");
+  }
+};
+
+Snake.prototype.move = () => {
+  const head = this.segments[0];
+  let newHead;
+  this.direction = this.nextDirection;
+
+  if (this.direction === "right") {
+    newHead = new Block(head.col + 1, head.row);
+  } else if (this.direction === "down") {
+    newHead = new Block(head.col, head.row + 1);
+  } else if (this.direction === "left") {
+    newHead = new Block(head.col - 1, head.row);
+  } else if (this.direction === "up") {
+    newHead = new Block(head.col, head.row - 1);
+  }
+
+  if (this.checkCollision(newHead)) {
+    gameOver();
+    return;
+  }
+
+  this.segments.unshift(newHead);
+
+  if (newHead.equal(apple.position)) {
+    score++;
+    apple.move();
+  } else {
+    this.segments.pop();
+  }
+};
+
+Snake.prototype.checkCollision = function (head) {
+  const leftCollision = head.col === 0;
+  const topCollision = head.row === 0;
+  const rightCollision = head.col === widthInBlocks - 1;
+  const bottomCollision = head.row === heightInBlocks - 1;
+  const wallCollision =
+    leftCollision || topCollision || rightCollision || bottomCollision;
+  const selfCollision = false;
+
+  for (let i = 0; i < this.segments.length; i++) {
+    if (head.equal(this.segments[i])) {
+      selfCollision = true;
+    }
+  }
+  return wallCollision || selfCollision;
+};
+
+const directions = {
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+};
+
+$("body").keydown(function (event) {
+  var newDirection = directions[event.keyCode];
+  if (newDirection !== undefined) {
+    Snake.setDirection(newDirection);
+  }
+});
+
+Snake.prototype.setDirection = (newDirection) => {
+  if (this.direction === "up" && newDirection === "down") {
+    return;
+  } else if (this.direction === "right" && newDirection === "left") {
+    return;
+  } else if (this.direction === "down" && newDirection === "up") {
+    return;
+  } else if (this.direction === "left" && newDirection === "right") {
+    return;
+  }
+  this.nextDirection = newDirection;
+};
+
+
