7ba17815c2c203af39ec4cc52d0505bc7e7799c5
Yara <cremaschi.yara@gmail.com>
2023-11-09 15:10:49 +0000

    added game logic

--- hangman/index.js
+++ hangman/index.js
@@ -1,30 +1,118 @@
+const wrongGuess = [];
+const rightGuess = [];
+const fruits = [
+  "acai",
+  "apple",
+  "apricot",
+  "avocado",
+  "banana",
+  "blueberry",
+  "blackberry",
+  "cherry",
+  "cranberry",
+  "currant",
+  "date",
+  "fig",
+  "grape",
+  "grapefruit",
+  "guava",
+  "melon",
+  "kiwi",
+  "lemon",
+  "lime",
+  "mango",
+  "orange",
+  "papaya",
+  "passion fruit",
+  "peach",
+  "pears",
+  "pineapple",
+  "plum",
+  "pomegranate",
+  "raspberry",
+  "strawberry",
+  "tamarind",
+  "tangerine",
+  "tomato",
+  "watermelons",
+];
 
+const secretWord = fruits[Math.floor(Math.random() * fruits.length)];
 
+document.addEventListener("keydown", (e) => {
+  const keyCode = e.keyCode;
+  if (isLetter(keyCode)) {
+    const letter = e.key;
 
-words[Math.floor(Math.random() * words.length)];
-
-const answerArr = [];
-
-for (let i = 0; i < word.length; i++) [(answerArr[i] = "_")];
-
-let remainingLetters = word.length;
-
-while (remainingLetters > 0) {
-  alert(answerArr.join(" "));
-  let guess = prompt("Guess a letter, or click Cancel to stop playing.");
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-  } else {
-    for(let i = 0; i < word.length; i++){
-        if(word[i] === guess){
-            answerArr[i] = guess
-            remainingLetters--
-        }
+    if (wrongGuess.includes(letter)) {
+      showWarning();
+    } else {
+      if (secretWord.includes(letter)) {
+        rightGuess.push(letter);
+      } else {
+        wrongGuess.push(letter);
+      }
     }
+    updateGame();
+  }
+});
+
+const updateGame = () => {
+  showWrongGuesses();
+  showRightGuesses();
+  drawHangman();
+  checkGameStatus();
+};
+
+const showWrongGuesses = () => {
+  const wrongGuessContainer = document.querySelector(".wrongGuess-container");
+  wrongGuessContainer.innerHTML = "";
+  wrongGuess.forEach((letter) => {
+    wrongGuessContainer.innerHTML += letter;
+  });
+};
+
+const showRightGuesses = () => {
+  const secretWordContainer = document.querySelector(".secretWord-container");
+  secretWordContainer.innerHTML = "";
+  secretWord.split("").forEach((letter) => {
+    if (rightGuess.includes(letter)) {
+      secretWordContainer.innerHTML += letter;
+    } else {
+      secretWordContainer.innerHTML += "_";
+    }
+  });
+};
+
+const checkGameStatus = () => {
+  const bodyParts = document.querySelectorAll(".body");
+  const secretWordContainer = document.querySelector(".secretWord-container")
+  if(wrongGuess.length === bodyParts.length){
+    alert(`You lost, the secret word was ${secretWord}`)
+    return;
+  }
+
+  if(secretWord === secretWordContainer.innerText) {
+    alert(`Yay! You guessed the secret word: ${secretWord}`)
+    return;
   }
 }
 
-alert(answerArr.join(" "));
-alert("Good job! The answer was " + word);
+const drawHangman = () => {
+  const bodyParts = document.querySelectorAll(".body");
+  for (let i = 0; i < wrongGuess.length; i++) {
+    bodyParts[i].style.display = "block";
+  }
+};
+
+const showWarning = () => {
+  alert("You've already tried that letter");
+};
+
+const isLetter = (keyCode) => {
+  return keyCode >= 65 && keyCode <= 90;
+};
+
+const restartGame = () => {
+  window.location.reload();
+}
