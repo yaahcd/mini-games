47166712fbff3aa70d1463566e056fa6bfb0913b
Yara <cremaschi.yara@gmail.com>
2023-11-10 13:17:33 +0000

    added some changes and img

--- treasure-hunt/index.js
+++ treasure-hunt/index.js
@@ -1,55 +1,51 @@
 const getRandomNumber = function (size) {
-    return Math.floor(Math.random() * size);
-  };
-
-  const getDistance = function (event, target) {
- 
-    let diffX = event.offsetX - target.x;
-    let diffY = event.offsetY - target.y;
-
-    return Math.sqrt(diffX * diffX + diffY * diffY);
-  };
-
-  const getDistanceHint = function (distance) {
-    if (distance < 10) {
-      return "Boiling hot!";
-    } else if (distance < 20) {
-      return "Really hot";
-    } else if (distance < 40) {
-      return "Hot";
-    } else if (distance < 80) {
-      return "Warm";
-    } else if (distance < 160) {
-      return "Cold";
-    } else if (distance < 320) {
-      return "Really cold";
-    } else {
-      return "Freezing!";
-    }
-
-  };
-
-  const width = 400;
-  const height = 400;
-  let clicks = 0;
-
-  const target = {
-    x: getRandomNumber(width),
-    y: getRandomNumber(height),
+  return Math.floor(Math.random() * size);
 };
 
+const getDistance = function (event, target) {
+  let diffX = event.offsetX - target.x;
+  let diffY = event.offsetY - target.y;
 
+  return Math.sqrt(diffX * diffX + diffY * diffY);
+};
 
-  $("#map").click(function (event) {
-    clicks++;
+const getDistanceHint = function (distance) {
+  if (distance < 10) {
+    return "Boiling hot!";
+  } else if (distance < 20) {
+    return "Really hot";
+  } else if (distance < 40) {
+    return "Hot";
+  } else if (distance < 80) {
+    return "Warm";
+  } else if (distance < 160) {
+    return "Cold";
+  } else if (distance < 320) {
+    return "Really cold";
+  } else {
+    return "Freezing!";
+  }
+};
 
-    let distance = getDistance(event, target);
-    let distanceHint = getDistanceHint(distance);
-    $("#distance").text(distanceHint);
+const width = 400;
+const height = 450;
+let clicks = 0;
 
-    console.log(distance);
+const target = {
+  x: getRandomNumber(width),
+  y: getRandomNumber(height),
+};
 
-    if (distance < 8) {
-      alert("Found the treasure in " + clicks + " clicks!");
-    }
-  });
+$("#map").click(function (event) {
+  clicks++;
+
+  let distance = getDistance(event, target);
+  let distanceHint = getDistanceHint(distance);
+  $("#distance").text(distanceHint);
+
+  if (distance < 8) {
+    alert("You've found the treasure in " + clicks + " clicks!");
+  }
+});
+
+console.log("aaaaa");
