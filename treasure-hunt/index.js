cf4863911f7fb4589e6f247f65d29cc8cc5f5201
Yara <cremaschi.yara@gmail.com>
2023-11-10 12:37:02 +0000

    started migrating minigame code from previous repo

---
+++ treasure-hunt/index.js
@@ -0,0 +1,55 @@
+const getRandomNumber = function (size) {
+    return Math.floor(Math.random() * size);
+  };
+
+  const getDistance = function (event, target) {
+ 
+    let diffX = event.offsetX - target.x;
+    let diffY = event.offsetY - target.y;
+
+    return Math.sqrt(diffX * diffX + diffY * diffY);
+  };
+
+  const getDistanceHint = function (distance) {
+    if (distance < 10) {
+      return "Boiling hot!";
+    } else if (distance < 20) {
+      return "Really hot";
+    } else if (distance < 40) {
+      return "Hot";
+    } else if (distance < 80) {
+      return "Warm";
+    } else if (distance < 160) {
+      return "Cold";
+    } else if (distance < 320) {
+      return "Really cold";
+    } else {
+      return "Freezing!";
+    }
+
+  };
+
+  const width = 400;
+  const height = 400;
+  let clicks = 0;
+
+  const target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height),
+};
+
+
+
+  $("#map").click(function (event) {
+    clicks++;
+
+    let distance = getDistance(event, target);
+    let distanceHint = getDistanceHint(distance);
+    $("#distance").text(distanceHint);
+
+    console.log(distance);
+
+    if (distance < 8) {
+      alert("Found the treasure in " + clicks + " clicks!");
+    }
+  });
