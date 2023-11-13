8abdd8f637da4b02563f5f26f132b2ea24a9b5fa
Yara <cremaschi.yara@gmail.com>
2023-11-13 16:43:38 +0000

    added more logic

--- noughts-and-crosses/index.js
+++ noughts-and-crosses/index.js
@@ -14,9 +14,6 @@ const possibleWins = [
 
 const startGame = () => {
   gameBoard = Array.from(Array(tableData.length).keys());
- 
-
- 
 };
 
 const setUserPreference = (e) => {
@@ -24,7 +21,77 @@ const setUserPreference = (e) => {
 };
 
 const makeMove = (e) => {
-  e.target.innerHTML = "test";
+  if (!userChoice) {
+    alert("Please choose one option to start the game");
+    return;
+  }
+
+  if (typeof gameBoard[e.target.id] === "number") {
+    e.target.innerHTML = userChoice;
+    gameBoard[e.target.id] = "human";
+
+    const checkGame = checkWinner(gameBoard, "human");
+
+    if (checkGame) {
+      gameOver(checkGame);
+    }
+  }
+
+  if (!checkTie()) {
+    computerMove(e);
+  } else {
+    alert("tie");
+  }
+};
+
+const checkWinner = (gameBoard, player) => {
+  let plays = gameBoard.reduce((acc, curr, index) => {
+    return curr === player ? acc.concat(index) : acc;
+  }, []);
+  let gameWon = false;
+  for (let [index, win] of possibleWins.entries()) {
+    if (win.every((element) => plays.indexOf(element) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
+};
+
+const gameOver = (gameWon) => {
+  for (let index of possibleWins[gameWon.index]) {
+    gameWon.player === "human" ? alert("you won!") : alert("you lost!");
+    break;
+  }
+
+  for (let i = 0; i < tableData.length; i++) {
+    tableData[i].removeEventListener("click", makeMove);
+  }
+};
+
+const computerMove = () => {
+  const move = document.getElementById(
+    gameBoard.find((element) => typeof element != "string")
+  );
+  const computerChoice = userChoice === "x" ? o : "x";
+  move.innerHTML = computerChoice;
+  gameBoard[gameBoard.find((element) => typeof element != "string")] =
+    "computer";
+  return;
+};
+
+const checkTie = () => {
+  const emptySquares = gameBoard.filter(
+    (element) => typeof element === "number"
+  );
+
+  if (emptySquares.length === 0) {
+    for (let i = 0; i < tableData.length; i++) {
+      tableData[i].removeEventListener("click", makeMove);
+    }
+    return true;
+  }
+  return false;
 };
 
 const tableData = document.querySelectorAll(".table-data");
@@ -37,4 +104,4 @@ for (let i = 0; i < userPreference.length; i++) {
   userPreference[i].addEventListener("click", setUserPreference);
 }
 
-startGame()
+startGame();
